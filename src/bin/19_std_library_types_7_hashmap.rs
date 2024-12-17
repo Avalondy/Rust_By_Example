29f6ecb630496b8f9c1d0ec2a8f98299ea087507
Avalondy <duyao2016@gmail.com>
2024-12-16 21:24:20 -0500

    Update Rust By Example chapter 19_7

--- src/bin/19_std_library_types_7_hashmap.rs
+++ src/bin/19_std_library_types_7_hashmap.rs
@@ -1,51 +1,81 @@
-use std::collections::HashMap;
+mod hash_map {
+    use std::collections::HashMap;
 
-#[derive(PartialEq, Eq, Hash)]
-struct Account<'a> {
-    username: &'a str,
-    password: &'a str,
-}
+    #[derive(PartialEq, Eq, Hash)]
+    struct Account<'a> {
+        username: &'a str,
+        password: &'a str,
+    }
 
-struct AccountInfo<'a> {
-    name: &'a str,
-    email: &'a str,
-}
+    struct AccountInfo<'a> {
+        name: &'a str,
+        email: &'a str,
+    }
 
-type Accounts<'a> = HashMap<Account<'a>, AccountInfo<'a>>;
+    type Accounts<'a> = HashMap<Account<'a>, AccountInfo<'a>>;
 
-fn try_logon(accounts: &Accounts, username: &str, password: &str) {
-    println!("Username: {}", username);
-    println!("Password: {}", password);
-    println!("Attempting logon...");
+    fn try_logon(accounts: &Accounts, username: &str, password: &str) {
+        println!("Username: {}", username);
+        println!("Password: {}", password);
+        println!("Attempting logon...");
 
-    let logon = Account { username, password };
+        let logon = Account { username, password };
 
-    match accounts.get(&logon) {
-        Some(account_info) => {
-            println!("Successful logon!");
-            println!("Name: {}", account_info.name);
-            println!("Email: {}", account_info.email);
+        match accounts.get(&logon) {
+            Some(account_info) => {
+                println!("Successful logon!");
+                println!("Name: {}", account_info.name);
+                println!("Email: {}", account_info.email);
+            }
+            None => println!("Login failed!"),
         }
-        None => println!("Login failed!"),
+    }
+
+    pub fn test() {
+        let mut accounts = HashMap::new();
+
+        let account = Account {
+            username: "j.everyman",
+            password: "password123",
+        };
+
+        let account_info = AccountInfo {
+            name: "John Everyman",
+            email: "j.everyman@email.com",
+        };
+
+        accounts.insert(account, account_info);
+
+        try_logon(&accounts, "j.everyman", "psasword123");
+
+        try_logon(&accounts, "j.everyman", "password123");
+    }
+}
+
+mod hash_set {
+    use std::collections::HashSet;
+
+    pub fn test() {
+        let mut a: HashSet<i32> = vec![1, 2, 3].into_iter().collect();
+        let mut b = vec![2, 3, 4].into_iter().collect::<HashSet<_>>();
+
+        assert!(a.insert(4));
+        assert!(a.contains(&4));
+
+        b.insert(5);
+
+        println!("A: {:?}", a);
+        println!("B: {:?}", b);
+
+        println!("Union: {:?}", a.union(&b));
+        println!("Difference: {:?}", a.difference(&b));
+        println!("Intersection: {:?}", a.intersection(&b));
+        println!("Symmetric Difference: {:?}", a.symmetric_difference(&b));
     }
 }
 
 fn main() {
-    let mut accounts = HashMap::new();
-
-    let account = Account {
-        username: "j.everyman",
-        password: "password123",
-    };
-
-    let account_info = AccountInfo {
-        name: "John Everyman",
-        email: "j.everyman@email.com",
-    };
-
-    accounts.insert(account, account_info);
-
-    try_logon(&accounts, "j.everyman", "psasword123");
-
-    try_logon(&accounts, "j.everyman", "password123");
+    hash_map::test();
+    println!();
+    hash_set::test();
 }
